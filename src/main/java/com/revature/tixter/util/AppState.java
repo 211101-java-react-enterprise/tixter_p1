268f4afe4690b77a9542065ce077c158881c5cf6
QiZhang-UConn <Qi.Zhang@uconn.edu>
2021-11-23 18:19:47 -0500

    added Object Models

---
+++ src/main/java/com/revature/tixter/util/AppState.java
@@ -0,0 +1,12 @@
+package com.revature.tixter.util;
+
+public class AppState {
+    protected boolean isRunning;
+
+    public AppState(){
+        this.isRunning=true;
+    }
+    public void initialState(){
+
+    }
+}
