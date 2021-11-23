eed90fd0cf60d617b372ddba2c8dbb49c89cbd6d
QiZhang-UConn <Qi.Zhang@uconn.edu>
2021-11-23 18:21:02 -0500

    delete appstate

--- src/main/java/com/revature/tixter/Tixter_Driver.java
+++ src/main/java/com/revature/tixter/Tixter_Driver.java
@@ -1,11 +1,7 @@
 package com.revature.tixter;
 
-import com.revature.tixter.util.AppState;
-
 public class Tixter_Driver {
     public static void main(String[] args) {
         System.out.println("Welcome To Tixter");
-        AppState appState=new AppState();
-        appState.initialState();
     }
 }
