8ae75b184e93cdec77f52efdce9b7e3d18f1d87a
QiZhang-UConn <Qi.Zhang@uconn.edu>
2021-11-26 10:51:32 -0500

    added placeholder for login and register servlet

---
+++ src/main/java/com/revature/tixter/web/servlets/RegisterServlet.java
@@ -0,0 +1,15 @@
+package com.revature.tixter.web.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RegisterServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException{
+        resp.getWriter().write("<h1>TODO: further implement Register Servlet</h1>");
+    }
+}
